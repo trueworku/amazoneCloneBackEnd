d52efdddcde4480f08733fe14941d105914260a7
true <trueworku@gmail.com>
2024-02-20 00:13:49 -0500

    modified the index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { onRequest } = require("firebase-functions/v2/https");
 const express = require ("express");
 const cors = require ("cors")
 const dotenv = require ("dotenv");
@@ -5,7 +6,7 @@ dotenv.config();
 const stripe = require ("stripe")(process.env.STRIPE_KEY);
 
 const app = express()
-app.use(cors({origin:true,credentials:true}))
+app.use(cors({origin:true,credentials:true}));
 
 app.use(express.json());
 
@@ -15,7 +16,7 @@ app.get("/",(req,res)=>{
     });
 });
 app.post("/payment/create", async(req,res)=>{
-    const total = req.query.total;
+    const total = parseInt(req.query.total);
 
     if(total>0){
        
@@ -40,4 +41,4 @@ if(err) throw err;
 console.log(`Amazone server Running on Port:http://localhost:${process.env.PORT}`);
 });
 
-
+exports.api = onRequest(app);
